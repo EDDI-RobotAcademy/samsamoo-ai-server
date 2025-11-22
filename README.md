229a8c36bec71f6390fc336cb81059afeec729dc
jspark2504 <parkjeongseo@naver.com>
2025-11-22 13:49:27 +0900

    feat: [박정서] 재무제표 문서 분석 시스템 진행을 위한 템플릿(Multi-Agent-UI)기반 세팅 추가 [SOF-1]

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# SamSamOO-AI-Server
+# samsamoo-ai-server
+samsamoo-ai-server
+
+
+
+## first .env.local setting (The top-level package)
+APP_HOST=0.0.0.0
+
+APP_PORT=33333
+
+AWS_ACCESS_KEY_ID=yourAccessKeyId
+
+AWS_SECRET_ACCESS_KEY=yourSecretAccessKey
+
+AWS_REGION=ap-northeast-2
+
+AWS_S3_BUCKET=yourS3Bucket
+
+MYSQL_USER=
+
+MYSQL_PASSWORD=
+
+MYSQL_HOST=
+
+MYSQL_PORT=
+
+MYSQL_DATABASE=
+
+
+REDIS_HOST=
+
+REDIS_PORT=
+
+REDIS_DB=
+
+REDIS_PASSWORD=
+
+
+GOOGLE_CLIENT_ID=yourClientId
+
+GOOGLE_CLIENT_SECRET=yourClientSecret
+
+GOOGLE_REDIRECT_URI=http://localhost:33333/authentication/google/redirect
+
+CLIENT_REDIRECT_URL=http://localhost:3000
+
+SESSION_TTL_SECONDS=3600
+
+
+# start
+ python -m app.main
---
+++ app/main.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+
+from anonymous_board.adapter.input.web.anonymous_board_router import anonymous_board_router
+from board.adapter.input.web.board_router import board_router
+from config.database.session import Base, engine
+from documents.adapter.input.web.documents_router import documents_router
+from documents_multi_agents.adapter.input.web.document_multi_agent_router import documents_multi_agents_router
+from social_oauth.adapter.input.web.google_oauth2_router import authentication_router
+
+load_dotenv()
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+os.environ["TORCH_USE_CUDA_DSA"] = "1"
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # Next.js 프론트 엔드 URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,       # 정확한 origin만 허용
+    allow_credentials=True,      # 쿠키 허용
+    allow_methods=["*"],         # 모든 HTTP 메서드 허용
+    allow_headers=["*"],         # 모든 헤더 허용
+)
+
+app.include_router(anonymous_board_router, prefix="/anonymouse-board")
+app.include_router(authentication_router, prefix="/authentication")
+app.include_router(board_router, prefix="/board")
+app.include_router(documents_router, prefix="/documents")
+app.include_router(documents_multi_agents_router, prefix="/documents-multi-agents")
+
+# 앱 실행
+if __name__ == "__main__":
+    import uvicorn
+    host = os.getenv("APP_HOST")
+    port = int(os.getenv("APP_PORT"))
+    # Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    uvicorn.run(app, host=host, port=port)
